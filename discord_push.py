fae844776b45ad851955ef048d36a4be421785de
zephyrj <zephyrj@protonmail.com>
2025-06-09 21:57:49 +0100

    Add position and site link to discord standings. Edit existing standings messages rather than added new ones

--- discord_push.py
+++ discord_push.py
@@ -9,7 +9,7 @@ MAX_POST_LEN = 1990
 def main():
     driver_standings = json.load(open('./data/seasons/season2/driver_standings.json', 'r', encoding='utf-8'))
     trimmed_standings = list()
-    for row in driver_standings["standings"]:
+    for (pos, row) in enumerate(driver_standings["standings"], 1):
         row_dict = dict()
         names = row["name"].rsplit(" ")
         last_name = names.pop()
@@ -17,21 +17,24 @@ def main():
         for name in names:
             abbr_name += name[0]+". "
         abbr_name += last_name
+        row_dict["pos"] = pos
         row_dict["name"] = abbr_name
         for k in ('team', 'championshipPoints', 'totalPoints'):
             row_dict[k] = row[k]
         trimmed_standings.append(row_dict)
 
     table = tabulate.tabulate(trimmed_standings,
-                              headers={'name': 'Driver',
+                              headers={'pos': 'Pos',
+                                       'name': 'Driver',
                                        'team': 'Team',
                                        "championshipPoints": 'Pts w/drop',
                                        "totalPoints": "All Pts"},
-                              maxcolwidths=[None, None, 4, 4],
-                              maxheadercolwidths=[None, None, 5, 5],
+                              maxcolwidths=[2, None, None, 4, 4],
+                              maxheadercolwidths=[3, None, None, 5, 5],
                               tablefmt="fancy_outline")
     lines = table.split('\n')
-    current_post_length = 0
+    main_site_link = os.getenv("SITE_URL"),
+    current_post_length = len(main_site_link) if main_site_link else 0
     posts = list()
     current_post_idx = 0
     posts.append([])
@@ -48,8 +51,13 @@ def main():
         t = '\n'.join(lines)
         webhook = discord_webhook.DiscordWebhook(url=os.getenv("WEBHOOK_URL"),
                                                  id=os.getenv(f"MESSAGE_{idx}_ID", None))
-        webhook.content=f"```{t}```"
-        webhook.execute()
+        content = ""
+        if main_site_link and idx == 0:
+            content += main_site_link + '\n'
+        content += f"```{t}```"
+        print(content)
+        webhook.content=content
+        webhook.edit()
 
 if __name__ == "__main__":
     main()
